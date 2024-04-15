836b07bf3182ab06b9952d6d58cea266c9a14baf
nati05 <83889620+Nataliamagali@users.noreply.github.com>
2024-04-15 10:40:37 -0400

    actualizado

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-console.log ("HOLA MUNDO");
-
-let mivariable = " hola mundo";
-
-console.log(mivariable);
-
-mivariable = 11;
-
-console.log(mivariable);
-
-const MICONSTANTE = "javascript";
-
-console.log(MICONSTANTE);
-
 function cualEsMayor(num1,num2,num3){
     if(num1 === num2 && num2 === num3){
         return 'Los tres numeros son iguales'
